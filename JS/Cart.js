02d569f70dffc1addc45bf6d1ceae6c4d568d291
Ahhhnope <magelova32@gmail.com>
2026-02-09 20:23:57 +0700

    added buttons functionality, price doesnt auto-update yet

--- JS/Cart.js
+++ JS/Cart.js
@@ -1,24 +1,27 @@
-var volume = document.getElementById('cart-number');
-var btn_add = document.getElementById('cart-increment');
-var btn_remove = document.getElementById('cart-deincrement');
-
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[maxlength]').forEach(input => {
-    input.addEventListener('input', e => {
-    let val = e.target.value, len = +e.target.getAttribute('maxlength');
-      e.target.value = val.slice(0,len);
+    document.querySelectorAll('[maxlength]').forEach(input => {
+        input.addEventListener('input', e => {
+            let val = e.target.value, len = +e.target.getAttribute('maxlength');
+            e.target.value = val.slice(0, len);
+        })
     })
-  })
 })
 
-btn_add.addEventListener("click", function() {
-    if (volume.value < volume.max) {
-        volume.value = Number(volume.value) + 1;
-    }
-});
+const cart_items = document.querySelectorAll('[id^="cart-item"]');
+// console.log(cart_items);
 
-btn_remove.addEventListener("click", function() {
-    if (volume.value > volume.min) {
-        volume.value = Number(volume.value) - 1;
-    }
+cart_items.forEach(item => {
+    let btn_add = item.querySelector('#cart-increment');
+    let btn_remove = item.querySelector('#cart-deincrement');
+    let item_volume = item.querySelector('#cart-number');
+
+    btn_add.addEventListener("click", function() {
+        if (item_volume.value < item_volume.max) {
+        item_volume.value = Number(item_volume.value) + 1;
+    }})
+
+    btn_remove.addEventListener("click", function() {
+        if (item_volume.value > item_volume.min) {
+        item_volume.value = Number(item_volume.value) - 1;
+    }})
 });
--- JS/CartOV.js
+++ JS/CartOV.js
@@ -1,6 +1,6 @@
-const volume = document.getElementById('cart-number');
-const btn_add = document.getElementById('cart-increment');
-const btn_remove = document.getElementById('cart-deincrement');
+// const volume = document.getElementById('cart-number');
+// const btn_add = document.getElementById('cart-increment');
+// const btn_remove = document.getElementById('cart-deincrement');
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[maxlength]').forEach(input => {
@@ -11,28 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-const btns_add = document.querySelectorAll('cart-increment');
-const btns_remove = document.querySelectorAll('cart-deincrement');
-const items_volume = document.querySelectorAll('cart-number');
+const cart_items = document.querySelectorAll('[id^="cart-item"]');
+// console.log(cart_items);
 
-for (let i = 0; i < btns_add.length; i++) {
-    console.log(i);
-    btns_add[i].addEventListener("click", function () {
-        if (items_volume[i].value < items_volume[i].max) {
-            items_volume[i].value = Number(items_volume[i].value) + 1;
-        }
-    });
-}
+cart_items.forEach(item => {
+    let btn_add = item.querySelector('#cart-increment');
+    let btn_remove = item.querySelector('#cart-deincrement');
+    let item_volume = item.querySelector('#cart-number');
 
+    btn_add.addEventListener("click", function() {
+        if (item_volume.value < item_volume.max) {
+        item_volume.value = Number(item_volume.value) + 1;
+    }})
 
-// btn_add.addEventListener("click", function () {
-//     if (volume.value < volume.max) {
-//         volume.value = Number(volume.value) + 1;
-//     }
-// });
+    btn_remove.addEventListener("click", function() {
+        if (item_volume.value > item_volume.min) {
+        item_volume.value = Number(item_volume.value) - 1;
+    }})
+});
 
-// btn_remove.addEventListener("click", function () {
-//     if (volume.value > volume.min) {
-//         volume.value = Number(volume.value) - 1;
-//     }
-// });
